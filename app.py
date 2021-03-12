387c07b1b24e255a1fbb80e5a7bac235a159ebd8
leithdm <leithdm@gmail.com>
2021-03-12 08:44:56 +0000

    fix: Set debug to False for deployment

--- app.py
+++ app.py
@@ -68,4 +68,6 @@ def register():
 
 # tell our app how and where to run our application
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=False)
