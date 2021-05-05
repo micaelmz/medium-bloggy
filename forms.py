12ed337f3411d782694d8c8cb671f3a67af4c954
leithdm <leithdm@gmail.com>
2021-05-05 07:37:13 +0100

    feat: Provide email validation

--- forms.py
+++ forms.py
@@ -2,16 +2,16 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField, validators
 from wtforms.validators import DataRequired, URL
 from flask_ckeditor import CKEditorField
-
+from wtforms.fields.html5 import EmailField
 
 class RegisterForm(FlaskForm):
-    email = StringField("Email", [validators.Length(min=4, max=20)])
+    email = EmailField("Email address", [validators.DataRequired(),validators.Email()])
     password = PasswordField('Password', [
         validators.DataRequired(),
         validators.EqualTo('confirm', message='Passwords must match')
     ])
     confirm = PasswordField('Repeat Password')
-    name = StringField("Name", validators=[DataRequired()])
+    name = StringField("Name", [validators.Length(min=4, max=25)])
     submit = SubmitField("Register")
 
 
