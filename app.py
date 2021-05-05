b74fdfa2f360a62b6016b5d3eac2acc0d2ae7874
leithdm <leithdm@gmail.com>
2021-05-05 14:42:57 +0100

    feat: Add additional HTTP status code pages

--- app.py
+++ app.py
@@ -195,6 +195,16 @@ def page_not_found(e):
     return render_template('404.html'), 404
 
 
+@app.errorhandler(403)
+def page_not_found(e):
+    return render_template('403.html'), 403
+
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('500.html'), 500
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
